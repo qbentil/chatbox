95702ebb23f905e73b43d9e65764a723b67edfa5
qbentil <Bentilshadrack72@gmail.com>
2022-02-16 13:24:30 +0000

    Update: SearchBar Components from .jsx to js

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,22 +1,20 @@
-import * as WebBrowser from 'expo-web-browser';
-
 import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Text, View } from './Themed';
 
 import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
-import { MonoText } from './StyledText';
 import Styles from '../constants/Styles';
+import tw from 'twrnc'
 import useColorScheme from '../hooks/useColorScheme';
 
-export default function SearchBar({ path }: { path: string }) {
+const SearchBar = (props) =>{
     const colorScheme = useColorScheme();
     return (
         <View style={[Styles.searchBar.container, {backgroundColor: Colors[colorScheme].backgroundOpac}]}>
-        <Ionicons name='search' size={25} color = {Colors[colorScheme].tabIconDefault} />
+        <Ionicons name='search' size={22} color = {Colors[colorScheme].tabIconDefault} />
         <TextInput
-            style={Styles.searchBar.input}
-            placeholder={path}
+            style={[tw``, Styles.searchBar.input]}
+            placeholder="Search"
             onChangeText={(searchString) => {console.log(searchString)}}
             underlineColorAndroid="transparent"
             placeholderTextColor={Colors[colorScheme].tabIconDefault}
@@ -25,7 +23,7 @@ export default function SearchBar({ path }: { path: string }) {
     );
 }
 
-
+export default SearchBar;
 const styles = StyleSheet.create({
     searchSection: {
     flex: 1,

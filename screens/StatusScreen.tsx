33bb23b53fa7afb87995ac0e3f38f39a5c6d5f69
qbentil <Bentilshadrack72@gmail.com>
2022-02-16 04:27:23 +0000

    Create Status Screen

---
+++ screens/StatusScreen.tsx
@@ -0,0 +1,31 @@
+import { Text, View } from '../components/Themed';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { StyleSheet } from 'react-native';
+
+export default function StatusScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab Two</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});

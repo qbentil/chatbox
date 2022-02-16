a3485d5749b0131dcf6548d860a35f4aa0d9ec2e
qbentil <Bentilshadrack72@gmail.com>
2022-02-16 04:26:46 +0000

    Create Styles constants to be used accross app

---
+++ constants/Styles.ts
@@ -0,0 +1,15 @@
+import Colors from './Colors';
+
+export default {
+    headerTitle: {
+        color: Colors.light.tint,
+        // fontWeight: 'bold', 
+        fontSize: 17
+    },
+    headerIconsContainer: {
+        flexDirection: 'row', 
+        width: 56, 
+        justifyContent: 'space-between', 
+        marginRight: 10
+    },
+}

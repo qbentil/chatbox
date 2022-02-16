eafd1fe171dc1e75169f510e56075bc9006b0429
qbentil <Bentilshadrack72@gmail.com>
2022-02-16 13:22:05 +0000

    Update: Add NewGroupModal to RootStackParamList

--- types.tsx
+++ types.tsx
@@ -16,6 +16,7 @@ declare global {
 
 export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
+  NewGroupModal: undefined;
   Modal: undefined;
   NotFound: undefined;
 };

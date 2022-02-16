7fcf343130a14c8528f8f7691b7a488805b8e321
qbentil <Bentilshadrack72@gmail.com>
2022-02-16 10:43:24 +0000

    feat: Create & Design SearchBar resusable template

---
+++ components/SearchBar.tsx
@@ -0,0 +1,52 @@
+import * as WebBrowser from 'expo-web-browser';
+
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View } from './Themed';
+
+import Colors from '../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { MonoText } from './StyledText';
+import Styles from '../constants/Styles';
+import useColorScheme from '../hooks/useColorScheme';
+
+export default function SearchBar({ path }) {
+    const colorScheme = useColorScheme();
+    return (
+        <View style={[Styles.searchBar.container, {backgroundColor: Colors[colorScheme].backgroundOpac}]}>
+        <Ionicons name='search' size={25} color = {Styles.searchBar.iconColor}/>
+        <TextInput
+            style={Styles.searchBar.input}
+            placeholder={path}
+            onChangeText={(searchString) => {console.log(searchString)}}
+            underlineColorAndroid="transparent"
+            placeholderTextColor={Styles.searchBar.input.color}
+        />
+    </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    searchSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginVertical: 10,
+    opacity: 0.3,
+    padding: 5,
+    paddingHorizontal: 10,
+    },
+    searchIcon: {
+        padding: 10,
+    },
+    input: {
+        flex: 1,
+        paddingVertical: 8,
+        paddingLeft: 10,
+        color: Colors.dark.text,
+        fontSize: 17,
+        fontWeight: '400',
+    },
+});

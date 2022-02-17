85c09878ff274fcdc9db4ce23753a71f0ede791e
qbentil <Bentilshadrack72@gmail.com>
2022-02-17 23:14:28 +0000

    Update: Design ChatListItem - Complete

--- components/chatListItem/Style.js
+++ components/chatListItem/Style.js
@@ -1,6 +1,40 @@
-export default {
-    avatar: {
-        height: 60,
-        width: 60
-    }
-}
+import { StyleSheet } from 'react-native';
+
+const Style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    width: "100%",
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  lefContainer: {
+    flexDirection: 'row',
+  },
+  midContainer: {
+    justifyContent: 'space-around'
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    marginRight: 15,
+    backgroundColor: 'gray'
+  },
+  username: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginTop: -10
+  },
+  lastMessage: {
+    fontSize: 16,
+    color: 'grey',
+    marginTop: -15
+  },
+  time: {
+    fontSize: 14,
+    color: 'grey',
+    marginTop: 6
+  },
+});
+
+export default Style;

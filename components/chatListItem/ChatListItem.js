09ee816c1253e527309e2ee82a1efbabf24962e3
qbentil <Bentilshadrack72@gmail.com>
2022-02-17 20:16:28 +0000

    Update: Implementing ChatListItem Component

--- components/chatListItem/ChatListItem.js
+++ components/chatListItem/ChatListItem.js
@@ -1,4 +1,4 @@
-import { FlatList, TextInput, TouchableOpacity } from 'react-native';
+import { FlatList, Image, TextInput, TouchableOpacity } from 'react-native';
 import { Text, View } from '../Themed';
 
 import { ChatRoom } from '../../types';
@@ -6,36 +6,37 @@ import ChatRooms from '../../data/ChatRooms';
 import Colors from '../../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
+import Style from './Style';
 import tw from 'twrnc'
 import useColorScheme from '../../hooks/useColorScheme';
-
-// import { Text, View } from 'react-native';
-
 
 const ChatListItem = (chatRoom) => {
 
     return (
-        <View style={tw`flex-1 justify-between`}>
+        <View style={tw``}>
             <FlatList
                 data={ChatRooms}
                 scrollEnabled={false}
                 keyExtractor={(item) => item.id}
-                renderItem = {({item}) => {
-                    {/* User image */}
-                    <View>
-                        <Text>Hello Bentil Shadrack </Text>
-                    </View>
+                renderItem = {({item}) => (
+                    <ScrollView style={tw`flex-row justify-between`}>
+                        <View style={tw`flex-row justify-around my-2`}>
+                            <Image 
+                                source={{ uri: item.users.imageUri }}
+                                style={[tw`bg-gray-500 rounded-full mr-2`, Style.avatar]} 
+                            />
+                            <View style={tw``}>
+                                <Text style={tw`text-lg font-semibold`}>{'Ben'}</Text>
+                                <Text>{item.lastMessage.content}</Text>
+                            </View>
+                        </View>
 
-                    {/* Content */}
-                    <View>
-
-                    </View>
-
-                    {/* timeStamp */}
-                    <View>
-
-                    </View>
-                }}
+                        {/* timeStamp */}
+                        <View>
+                            <Text>yesterday</Text>
+                        </View>
+                    </ScrollView>
+                )}
             />
         </View>
     )

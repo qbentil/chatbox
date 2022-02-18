38d7c8cd608345fdaa803769e57f3d05f5465da7
qbentil <Bentilshadrack72@gmail.com>
2022-02-18 12:30:02 +0000

    Fix Bug: Fix ChatListIyems render bug from Day3

--- components/chatListItem/ChatListItem.tsx
+++ components/chatListItem/ChatListItem.tsx
@@ -1,7 +1,7 @@
+import { Image, TouchableOpacity } from 'react-native';
 import { Text, View } from '../Themed';
 
 import { ChatRoom } from '../../types';
-import { Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 import Style from './Style';
 import moment from 'moment';
@@ -10,18 +10,18 @@ import tw from 'twrnc'
 export type ChatListItemProp = {
     chatRoom: ChatRoom;
 }
-const ChatListItem = (props: ChatRoom) => {
+const ChatListItem = (props: ChatListItemProp) => {
     const { chatRoom } = props;
     const user = chatRoom.users[1];
     return (
         <TouchableOpacity style={Style.container}>
-        <View style={Style.lefContainer}>
-          <Image 
-            source={{ 
-                uri: user.imageUri
-            }} 
-            style={Style.avatar}
-        />
+          <View style={Style.lefContainer}>
+            <Image 
+              source={{ 
+                  uri: user.imageUri
+              }} 
+              style={Style.avatar}
+          />
 
           <View style={{}}>
             <Text style={Style.username}>{user.name}</Text>
@@ -40,6 +40,5 @@ const ChatListItem = (props: ChatRoom) => {
       </TouchableOpacity>
     )
 }
-// moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")
 
 export default ChatListItem;

4af8a87de4692c1fd4c13b70cde70e22d8a5abfe
qbentil <Bentilshadrack72@gmail.com>
2022-02-18 01:56:56 +0000

    Fix bug: Chatroom Broken links

--- data/ChatRooms.js
+++ data/ChatRooms.js
@@ -92,13 +92,13 @@ export default [
       imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
     }, {
       id: 'u7',
-      name: 'Adrian',
-      imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-8/12185020_10206884996416284_5116038671917824834_o.jpg?_nc_cat=101&_nc_sid=174925&_nc_eui2=AeGr_NhJtwGWWfHbkoBSmifLKzIg47DpEF0rMiDjsOkQXf7yavDd4m-3CLbl5nqAMGcj2nn0Dqg7beNAzuZMzTFK&_nc_ohc=KOCn_AYCJxkAX_ZcLBP&_nc_ht=scontent.fkiv3-1.fna&oh=8cc4e5445ce4633db9d6234f42ccb368&oe=5F9C24D1',
+      name: '@qBentil',
+      imageUri: 'https://codersquiz.netlify.app/img/bentil.jpeg',
     }],
     lastMessage: {
       id: 'm6',
       content: 'I have a solution',
-      createdAt: '2020-10-02T15:40:00.000Z',
+      createdAt: '2022-02-17T15:40:00.000Z',
     }
   }, 
   {

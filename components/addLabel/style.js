d36b1b37814b3b72bf9b8fb8305f37205b1e1fd9
qbentil <Bentilshadrack72@gmail.com>
2022-02-19 17:19:05 +0000

    Feat✔:  Create add Label Pages

---
+++ components/addLabel/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+export const style = StyleSheet.create({
+    container: {
+        
+    },
+    colorPicker: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        backgroundColor: 'gray'
+    }
+})

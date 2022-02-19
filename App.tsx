c7618b8638d538dd24163ca71f9befa4c281366b
qbentil <Bentilshadrack72@gmail.com>
2022-02-19 12:30:54 +0000

    Fix 🐛:  Nested Virtual List bug

--- App.tsx
+++ App.tsx
@@ -4,10 +4,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { useEffect } from 'react';
+import { LogBox } from 'react-native';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  
+  useEffect(() => {
+    LogBox.ignoreLogs(["VirtualizedLists should never be nested"])
+  }, [])
 
   if (!isLoadingComplete) {
     return null;

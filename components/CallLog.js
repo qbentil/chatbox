d9ce5f08fc9161843838583eb63099c867ba9415
qbentil <Bentilshadrack72@gmail.com>
2022-02-21 22:33:52 +0000

    Update✨✔ : Sort Call Log Data

--- components/CallLog.js
+++ components/CallLog.js
@@ -3,12 +3,12 @@ import CallsData from '../data/CallsData';
 import {FlatList} from 'react-native';
 
 const CallLog = () => {
-
+    const SortedCallsData = CallsData.slice().sort((a, b) => new Date(b.date) - new Date(a.date))
     return (
 
         <FlatList 
             scrollEnabled = {false}
-            data={CallsData}
+            data={SortedCallsData}
             style={{width: "100%"}}
             keyExtractor = {(item) => item.id}
             renderItem = {({item}) => (

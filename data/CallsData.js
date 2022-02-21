5d01cca6ab2f3189a360c5a45ba3194edefdf0fc
qbentil <Bentilshadrack72@gmail.com>
2022-02-21 22:13:17 +0000

    Update✨: Add more dummy Data to calls

--- data/CallsData.js
+++ data/CallsData.js
@@ -8,7 +8,7 @@ export default [
             name: '@qBentil',
             imageUri: 'https://codersquiz.netlify.app/img/bentil.jpeg',
         },
-        date: '2020-10-19T15:40:00.000Z'
+        date: '2022-02-21T15:40:00.000Z'
     },
     {
         id: 'C2',
@@ -22,7 +22,7 @@ export default [
         date: '2020-10-20T15:40:00.000Z'
     },
     {
-        id: 'C2',
+        id: 'C3',
         mode: 'audio',
         type: 'Outgoing',
         user: {
@@ -30,10 +30,10 @@ export default [
             name: '@qBentil',
             imageUri: 'https://codersquiz.netlify.app/img/bentil.jpeg',
         },
-        date: '2022-02-21T15:40:00.000Z'
+        date: '2022-02-19T15:40:00.000Z'
     },
     {
-        id: 'C3',
+        id: 'C4',
         mode: 'audio',
         type: 'Missed',
         user: {
@@ -42,5 +42,27 @@ export default [
             imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
         },
         date: '2022-02-20T15:40:00.000Z'
-    }
+    },
+    {
+        id: 'C5',
+        mode: 'video',
+        type: 'Incoming',
+        user: {
+            id: 'u10',
+            name: 'Angelina Jolie',
+            imageUri: 'https://lkbkspro.s3.amazonaws.com/atelier-management/gs_58d933b8-98b4-468e-b229-43100a9620a7.jpg',
+          },
+        date: '2022-02-19T15:40:00.000Z'
+    },
+    {
+        id: 'C6',
+        mode: 'video',
+        type: 'Incoming',
+        user: {
+            id: 'u9',
+            name: 'Mom',
+            imageUri: 'https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg',
+          },
+        date: '2020-10-20T15:40:00.000Z'
+    },
 ]

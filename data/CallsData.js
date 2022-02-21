55bcaf455d3663686f53e318dcc77fc800af120d
qbentil <Bentilshadrack72@gmail.com>
2022-02-21 15:13:07 +0000

    Add DUmm Call  data

---
+++ data/CallsData.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        id: 'C1',
+        mode: 'video',
+        type: 'Missed',
+        user:{
+            id: 'u7',
+            name: '@qBentil',
+            imageUri: 'https://codersquiz.netlify.app/img/bentil.jpeg',
+        },
+        date: '2020-10-19T15:40:00.000Z'
+    },
+    {
+        id: 'C2',
+        mode: 'video',
+        type: 'Incoming',
+        user: {
+            id: 'u1',
+            name: 'Vadim',
+            imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
+        },
+        date: '2020-10-20T15:40:00.000Z'
+    },
+    {
+        id: 'C2',
+        mode: 'audio',
+        type: 'Outgoing',
+        user: {
+            id: 'u7',
+            name: '@qBentil',
+            imageUri: 'https://codersquiz.netlify.app/img/bentil.jpeg',
+        },
+        date: '2022-02-21T15:40:00.000Z'
+    },
+    {
+        id: 'C3',
+        mode: 'audio',
+        type: 'Missed',
+        user: {
+            id: 'u1',
+            name: 'Vadim',
+            imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
+        },
+        date: '2022-02-20T15:40:00.000Z'
+    }
+]

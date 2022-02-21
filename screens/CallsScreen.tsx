8037d1060e4fe433a80baf33d44b347adb9c042f
qbentil <Bentilshadrack72@gmail.com>
2022-02-21 15:12:04 +0000

    Feate✔: Create and Render CallScreenHeadfer component

--- screens/CallsScreen.tsx
+++ screens/CallsScreen.tsx
@@ -1,25 +1,31 @@
 import { Text, View } from '../components/Themed';
 
-import EditScreenInfo from '../components/EditScreenInfo';
+import CallListItem from '../components/callListItem';
+import CallScreenHeader from '../headers/CallScreenHeader';
+import CallsData from '../data/CallsData';
+import SearchBar from '../components/searchBar';
 import { StyleSheet } from 'react-native';
 
 export default function CallsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Call Tab</Text>
+      <CallScreenHeader />
+      <Text style = {[styles.title]}>Calls</Text>
+      <SearchBar />
+      {/* <CallListItem call={CallsData[0]} /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 15,
+    minHeight: 800,
+    flex: 1
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 35,
+    fontWeight: '600',
   },
   separator: {
     marginVertical: 30,

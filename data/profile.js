f47cea28f59807ec5c38dcda4ee27a397337f1bd
qbentil <Bentilshadrack72@gmail.com>
2022-02-23 11:44:42 +0000

    Update➕: Add dummy profile Datas

---
+++ data/profile.js
@@ -0,0 +1,6 @@
+export default {
+    id: 'u1',
+    name: 'BentilZone Web Hosting',
+    imageUri: 'https://raw.githubusercontent.com/qbentil/qbentil/main/BentilZone-logo-08.png',
+    status: "Servers you can trust",
+}

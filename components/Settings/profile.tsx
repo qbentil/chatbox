eac3a29d76998c8d70a9e2a3a8af06f2981c2076
qbentil <Bentilshadrack72@gmail.com>
2022-02-23 11:48:55 +0000

    Update✨: Create Profile Tab

---
+++ components/Settings/profile.tsx
@@ -0,0 +1,34 @@
+
+import { Image, TouchableOpacity , View} from 'react-native';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Text } from '../Themed';
+
+import React from 'react';
+import tw from 'twrnc'
+import profile from '../../data/profile';
+import { styles } from './style';
+import Colors from '../../constants/Colors';
+import useColorScheme from '../../hooks/useColorScheme';
+
+const Profile = () => {
+    const colorScheme = useColorScheme()
+    return (
+        <View style = {[tw`flex-row justify-between items-center my-3 bg-gray-900 py-4 px-2 border-t border-b`, {borderColor: Colors[colorScheme].backgroundOpac, borderTopWidth: 0.3, borderBottomWidth: 0.3}]}>
+            <View style = {tw`flex-row justify-between items-center w-2/3`}>
+                <Image 
+                    source={{uri: profile.imageUri}}
+                    style = {styles.avatar}
+                />
+                <View>
+                    <Text numberOfLines={2} ellipsizeMode = {'tail'}  style = {styles.username}>{profile.name}</Text>
+                    <Text numberOfLines={1} ellipsizeMode = {'tail'}  style = {styles.status}>{profile.status}</Text>
+                </View>
+            </View>
+            <TouchableOpacity activeOpacity={.7} style = {[tw`flex items-center mx-3`, styles.icon, {backgroundColor: Colors[colorScheme].backgroundOpac}]}>
+                <Ionicons name="ios-qr-code-sharp" style = {tw`mt-2`} size={24} color={Colors.light.tint} />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Profile;

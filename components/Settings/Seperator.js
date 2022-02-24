cf560183b3b739a1acb9ac6930de9fbad260f008
qbentil <Bentilshadrack72@gmail.com>
2022-02-24 12:18:08 +0000

    Create Seperator for Items

---
+++ components/Settings/Seperator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import tw from 'twrnc'
+
+const Seperator = () => {
+  return (
+    <View style={tw`my-4`}></View>
+  )
+}
+
+export default Seperator

cd12b30ed62faa5250c5234e84f88c74355afe5e
qbentil <Bentilshadrack72@gmail.com>
2022-03-03 11:15:04 +0000

    Style QR Scan Page Layout

--- screens/QRScreen.tsx
+++ screens/QRScreen.tsx
@@ -18,8 +18,15 @@ export default function QRScreen({ navigation }: RootTabScreenProps<'Labels'>) {
             source={{uri: profile.imageUri}}
             style = {styles.avatar}                    
           />
-          <View style = {tw`w-90 h-90 rounded-xl mt-32 ${colorScheme == 'light'? 'bg-gray-200': 'bg-[#0D0D0D]'}`}>
-    	        <Text>{profile.name}</Text>
+          <View style = {tw`w-90 absolute items-center py-5  h-90 rounded-xl mt-32 ${colorScheme == 'light'? 'bg-gray-200': 'bg-gray-800'}`}>
+    	        <Text style={tw`font-bold text-xl my-2`}>{profile.name}</Text>
+              <Text style={tw` text-gray-500 text-sm`}>WhatsApp Business Account</Text>
+              <View style ={tw`p-4 bg-white rounded-lg mt-5`}>
+                <Image 
+                    source={{uri: "https://twilio-cms-prod.s3.amazonaws.com/images/sample_whatsapp_qr_code.width-800.png"}}
+                    style = {[tw`rounded-lg`,{width: 200, height: 200}]}                    
+                  />
+              </View>
           </View>
         </View>
     </View>
@@ -34,13 +41,16 @@ const styles = StyleSheet.create({
     
   },    
   avatar: {
-    width: 60,
-    height: 60,
+    width: 80,
+    height: 80,
     borderRadius: 50,
     marginRight: 15,
     backgroundColor: 'white',
-    // padding: 25
-    
+    borderWidth: 1,
+    borderColor: "#ccc",
+    position: 'absolute',
+    top: 70,
+    zIndex: 999,
 }
 
 });

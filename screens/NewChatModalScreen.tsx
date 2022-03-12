932522df8089596b187f4a2a501252fbe09ae178
qbentil <Bentilshadrack72@gmail.com>
2022-03-12 19:17:10 +0000

    Feat: Setup New Call Modal Screen

---
+++ screens/NewChatModalScreen.tsx
@@ -0,0 +1,32 @@
+import { Platform, StyleSheet } from 'react-native';
+import { Text, View } from '../components/Themed';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { StatusBar } from 'expo-status-bar';
+
+export default function NewChatModalScreen() {
+  return (
+    <View style={styles.container}>
+
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});

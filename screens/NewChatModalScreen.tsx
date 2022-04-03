d08e6af197054a1d55223f691d503a4fb612aa5b
qbentil <Bentilshadrack72@gmail.com>
2022-04-03 19:24:18 +0000

    [Update]✔: Designing new chat modal screen

--- screens/NewChatModalScreen.tsx
+++ screens/NewChatModalScreen.tsx
@@ -1,17 +1,26 @@
-import { Platform, StyleSheet } from 'react-native';
-import { Text, View } from '../components/Themed';
+import { Platform, StyleSheet, View } from 'react-native';
 
 import NewChatModalHeader from '../headers/NewChatModalHeader';
+import NewChatNavs from '../components/NewChatNav';
 import SearchBar from '../components/searchBar';
 import { StatusBar } from 'expo-status-bar';
+import { Text } from '../components/Themed';
+import tw from 'twrnc';
+import useColorScheme from '../hooks/useColorScheme';
 
 export default function NewChatModalScreen() {
+  const colorScheme = useColorScheme();
   return (
     <View style={styles.container}>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
       {/* <NewChatModalHeader /> */}
-      <SearchBar />
+      <View style = {tw`w-full px-3 bg-${colorScheme == 'light'? 'white': '[#333333]'}`}>
+        <SearchBar style = {tw`bg-[#3b3b3b]`} />
+      </View>
+      <View style = {tw`w-full`}>
+        <NewChatNavs />
+      </View>
       
     </View>
   );

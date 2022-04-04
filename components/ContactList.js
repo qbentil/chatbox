844f044175833d86c412ba1b19ecfd990112b858
qbentil <Bentilshadrack72@gmail.com>
2022-04-04 16:23:00 +0000

    [Feate] Create a Contact List

---
+++ components/ContactList.js
@@ -0,0 +1,22 @@
+import {FlatList, ScrollView} from 'react-native';
+
+import ChatListItem from './chatListItem';
+import ChatRooms from '../data/ChatRooms';
+import FlatListView from './Lists/FlatList';
+import SectionListView from './Lists/SectionList';
+import Users from '../data/Users';
+
+const ContactList = () => {
+    const SortedChatData = ChatRooms.slice().sort((a, b) => new Date(b.lastMessage.CreatedAt) - new Date(a.lastMessage.CreatedAt));
+    // const SortedUsers = Users.slice().sort((a, b) => a - b);
+
+    return (
+
+        <FlatListView data={SortedChatData} />
+        // <SectionListView contacts={Users} />
+
+    );
+
+}
+
+export default ContactList;

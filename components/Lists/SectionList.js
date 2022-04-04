fc5ec63d2d7c923fc16fbd8f3e43d20a7bf180ae
qbentil <Bentilshadrack72@gmail.com>
2022-04-04 18:58:29 +0000

    FIx🐛: Resolve bug in List component

--- components/Lists/SectionList.js
+++ components/Lists/SectionList.js
@@ -1,12 +1,13 @@
-import {SectionList, Text} from 'react-native'
-
 import PropTypes from 'prop-types'
 import React from 'react'
+import {SectionList} from 'react-native'
+import SectionTitle from '../NewChatSectionTitle';
+import { Text } from '../Themed';
 import UserListItem from '../UsersListItem';
 
 const renderItem = ({item}) => <UserListItem user = {item} />
 
-const renderSectionHeader = ({section}) => <Text>{section.title}</Text>
+const renderSectionHeader = ({section}) => <SectionTitle title = {section.title} />
 
 const SectionListView = ({contacts}) => {
   const contactsByLetter = contacts.reduce((obj, contact) => {

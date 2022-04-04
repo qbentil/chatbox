0381b5665266239265f216af4a0cbea2dbd51eee
qbentil <Bentilshadrack72@gmail.com>
2022-04-04 18:59:28 +0000

    [Update]✔: SectionTitle to fit text

--- components/NewChatSectionTitle/index.js
+++ components/NewChatSectionTitle/index.js
@@ -6,7 +6,7 @@ import tw from 'twrnc'
 export default function SectionTitle({title}) {
   return (
     <View style={tw`w-full bg-[#3b3b3b] p-2`}>
-        <Text style = {tw`uppercase font-semibold text-sm`}>{title}</Text>
+        <Text style = {tw`uppercase font-semibold text-[12px] ml-3`}>{title}</Text>
     </View>
   )
 }

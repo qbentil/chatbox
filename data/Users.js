43c7cc10a1836b2a145220e6840b9fa3034b4e11
qbentil <Bentilshadrack72@gmail.com>
2022-04-05 05:03:40 +0000

    [Update]👀

--- data/Users.js
+++ data/Users.js
@@ -1,7 +1,7 @@
 export default [
   {
     id: 'u1',
-    name: '@AllyTyson',
+    name: 'AllyTyson',
     imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
     status: "Console.WriteLine('Please can I get a cup of code?');"
   }, {

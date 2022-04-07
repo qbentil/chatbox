cb293ab4d81a609aec40223bfd0c6945bc004df5
qbentil <Bentilshadrack72@gmail.com>
2022-04-07 12:25:26 +0000

    Update🔗: Setup Status Screen

--- screens/StatusScreen.tsx
+++ screens/StatusScreen.tsx
@@ -1,29 +1,54 @@
+import { ScrollView, StyleSheet, TextInput } from 'react-native';
 import { Text, View } from '../components/Themed';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { StyleSheet } from 'react-native';
+import Colors from '../constants/Colors';
+import { RootTabScreenProps } from '../types';
+import SearchBar from '../components/searchBar';
+import StatusProfile from '../components/Status';
+import StatusScreenHeader from '../headers/StatusScreenHeader';
+import useColorScheme from '../hooks/useColorScheme';
+import { useState } from 'react';
 
-export default function StatusScreen() {
+export default function StatusScreen({ navigation }: RootTabScreenProps<'Chats'>) {
+  const colorScheme = useColorScheme();
+  const [titleShown, setTitleShown] = useState(false)
+  const handleScroll = function(event: Object) {
+    event.nativeEvent.contentOffset.y <= 0 ? setTitleShown(false): setTitleShown(true)
+  }
+
+  const onSearch = (name:string) => {
+    console.log(name);
+    
+  }
+  
   return (
-    <View style={styles.container}>
-        
+    <View style={[styles.container, {backgroundColor: Colors[colorScheme].background}]}>
+        <StatusScreenHeader showTitle = {titleShown} />
+        <ScrollView  
+          onScroll={handleScroll} 
+          scrollEventThrottle = {1}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          nestedScrollEnabled
+        >
+           {!titleShown?  <Text style = {[styles.title]}>Status</Text>: null}
+            <SearchBar search = {onSearch} style = {{backgroundColor: Colors[colorScheme].backgroundOpac}} />
+            <StatusProfile />
+        </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 15,
+    minHeight: 800,
+    flex: 1
+    
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 35,
+    fontWeight: '600',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+
 });

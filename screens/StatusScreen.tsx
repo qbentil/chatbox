814ab82ba48fced804ce831b06a32e3778990ae4
qbentil <Bentilshadrack72@gmail.com>
2022-04-07 11:54:22 +0000

    update: Cleanup template for status screen

--- screens/StatusScreen.tsx
+++ screens/StatusScreen.tsx
@@ -6,9 +6,7 @@ import { StyleSheet } from 'react-native';
 export default function StatusScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+        
     </View>
   );
 }

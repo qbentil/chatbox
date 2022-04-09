e5cc160f23f78a7bc9659e543e42e76112a896b9
qbentil <Bentilshadrack72@gmail.com>
2022-04-09 13:46:00 +0000

    [Feat]: Create a dummy StatusUpdate data

---
+++ data/StatusUpdates.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        userID: 1,
+        updates: [
+            {
+                id: 's1',
+                types: 'text',
+                content: 'I am like the air, you no fit catch me \n ~Wale',
+                caption: null,
+                createdAt: '2022-02-20T15:40:00.000Z',
+            },
+            {
+                id: 's2',
+                types: 'photo',
+                content: 'https://codersquiz.netlify.app/img/bentil.jpeg',
+                caption: 'For God is',
+                createdAt: '2022-02-20T15:40:00.000Z',
+            },
+        ]
+    }
+]

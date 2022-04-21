fb3962e662ef9fa703bc5f06985e7063e3eedc15
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-04-21 09:06:17 +0000

    ChatRoomMessage styles

---
+++ components/chatMessage/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+    container: {
+      padding: 10,
+        
+    },
+    msgBox: {
+        marginRight: 50,
+        borderRadius: 5,
+        padding: 10,
+        
+    }
+
+})
+
+
+export default Styles;

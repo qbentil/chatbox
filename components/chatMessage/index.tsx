82a6093d36d70b1fc7d16ebb8e402f3b5e4a160a
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-04-21 09:05:39 +0000

    Create function to check incoming and outgoing messages

--- components/chatMessage/index.tsx
+++ components/chatMessage/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View } from '../Themed';
 
 import React from 'react';
+import Styles from './style';
 import { chatMessageProps } from '../../types';
 import moment from 'moment';
+import tw from 'twrnc'
 
 // import { Text, View } from 'react-native';
 
@@ -10,13 +12,25 @@ import moment from 'moment';
 
 
 
+
+
 const ChatMessage = (props: chatMessageProps) => {
     const {message} = props;
+    
+    // Check for incoming and outgoing messages
+    const isMyMessage = () => message.user.id === "u1";
+    
     return (
-        <View>
-            <Text>{message.user.name}</Text>
-            <Text>{message.content}</Text>
-            <Text>{moment(message.createdAt).fromNow()}</Text>
+        <View style = {[Styles.container]}>
+            <View style = {[
+                Styles.msgBox,
+                {backgroundColor: isMyMessage()? '#075E54': '#28282B'}
+                
+            ]}>
+                <Text>{message.user.name}</Text>
+                <Text style = {tw``}>{message.content}</Text>
+                <Text style = {tw`text-[0.65rem] -p-1 absolute right-0 bottom-0 pt-2 mr-2 text-gray-500  transition-all duration-100 group-hover:inline-flex`}>{moment(message.createdAt).fromNow()}</Text>
+            </View>
         </View>
     )
 }

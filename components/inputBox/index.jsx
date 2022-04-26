075e656ebad2938fd5706ad1efef32d95baef21f
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-04-26 07:59:43 +0000

    Structuring imput box

--- components/inputBox/index.jsx
+++ components/inputBox/index.jsx
@@ -1,11 +1,23 @@
+import { KeyboardAvoidingView, TextInput } from 'react-native'
 import { Text, View } from '../Themed'
 
+import Colors from '../../constants/Colors'
+import { Feather } from '@expo/vector-icons'
 import React from 'react'
+import tw from 'twrnc'
 
 export default function InputBox() {
   return (
-    <View>
-      <Text>InputBox</Text>
-    </View>
+    <KeyboardAvoidingView>
+      <View>
+        <Feather name="plus" size={24} color={Colors.light.tint} />
+      </View>
+      <View>
+        <TextInput />
+      </View>
+      <View>
+        
+      </View>
+    </KeyboardAvoidingView>
   )
 }

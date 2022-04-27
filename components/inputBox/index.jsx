5f053a44b98eb8c89249b81137f3727bccb35354
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-04-27 10:22:58 +0000

    Fix ChatBox input heights

--- components/inputBox/index.jsx
+++ components/inputBox/index.jsx
@@ -1,33 +1,39 @@
 import { Feather, MaterialIcons, SimpleLineIcons } from '@expo/vector-icons'
-import { Keyboard, KeyboardAvoidingView, Platform, TextInput, TouchableNativeFeedback } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, Platform, TextInput, TouchableNativeFeedback, TouchableOpacity } from 'react-native'
 import { Text, View } from '../Themed'
 
 import Colors from '../../constants/Colors'
 import React from 'react'
 import tw from 'twrnc'
+import useColorScheme from '../../hooks/useColorScheme'
 
 export default function InputBox() {
+  colorScheme = useColorScheme()
   return (
     <KeyboardAvoidingView 
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
       <TouchableNativeFeedback onPress={Keyboard.dismiss} >
         <View style = {tw`flex flex-row justify-between px-5 py-3 w-100 items-end`}>
-          <View style={{}}>
+          <TouchableOpacity style = {tw`mb-1`}>
             <Feather name="plus" size={24} color={Colors.light.tint} />
-          </View>
+          </TouchableOpacity>
           <View>
             <TextInput 
               multiline
               numberOfLines={4}
               onChangeText={(text) => console.log(text)}
               returnKeyType= {"return"}
-              style={tw`p-2 border-2 border-gray-900 w-70 min-h-10 text-[#eee] max-h-30 rounded-2xl`}
+              style={tw`p-2 border border-[${Colors[colorScheme].backgroundOpac}] w-70 min-h-8 text-[#eee] max-h-30 rounded-2xl `}
             />
           </View>
-          <View style = {tw`flex flex-row justify-between w-[40px]`}>
-            <SimpleLineIcons name="camera" size={24} color={Colors.light.tint} style = {tw`mx-1`} />
-            <MaterialIcons name="keyboard-voice" size={24} color={Colors.light.tint} style = {tw`mx-1`} />
+          <View style = {tw`flex flex-row justify-between w-[40px] mb-1`}>
+            <TouchableOpacity style = {tw`mx-1`} >
+              <SimpleLineIcons name="camera" size={24} color={Colors.light.tint} />
+            </TouchableOpacity>
+            <TouchableOpacity style = {tw`mx-1`} >
+              <MaterialIcons name="keyboard-voice" size={24} color={Colors.light.tint} />
+            </TouchableOpacity>
           </View>
         </View>
       </TouchableNativeFeedback>

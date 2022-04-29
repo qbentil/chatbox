4327101f2473c0463e54c62d5ccc87bba2436ca3
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-04-29 09:53:32 +0000

    Fix  Sticker btn positioning on  input box

--- components/inputBox/index.jsx
+++ components/inputBox/index.jsx
@@ -18,7 +18,7 @@ export default function InputBox() {
     >
       <TouchableNativeFeedback onPress={Keyboard.dismiss} >
         <View style = {tw`flex flex-row justify-between px-5 py-3 w-100 items-end`}>
-          <TouchableOpacity style = {tw`mb-1`}>
+          <TouchableOpacity activeOpacity={.7} style = {tw`mb-1`}>
             <Feather name="plus" size={24} color={Colors.light.tint} />
           </TouchableOpacity>
           <View style = {tw`w-${chatMsg.length <=0? '70': '80'}`}>
@@ -35,21 +35,21 @@ export default function InputBox() {
               keyboardType='ascii-capable'
               style={tw`p-2 border border-[${Colors[colorScheme].backgroundOpac}] w-full min-h-8 text-[#eee] max-h-30 rounded-2xl `}
             />
-            <TouchableOpacity style={tw`absolute right-0 mr-2`}>
-              <MaterialCommunityIcons name="sticker-outline" size={24} style={tw`mt-[6px]`} color={Colors.light.tint} />
-            </TouchableOpacity>
           </View>
+          <TouchableOpacity activeOpacity={.7} style={tw`absolute right-${chatMsg.length <=0 ? '18':'10'} bottom-4 mr-1`}>
+              <MaterialCommunityIcons name="sticker-outline" size={24} style={tw`mt-[6px]`} color={Colors.light.tint} />
+          </TouchableOpacity>
           { chatMsg.length <=0 ? (
           <View style = {tw`flex flex-row justify-between w-[40px] mb-1`}>
-            <TouchableOpacity style = {tw`mx-1`} >
+            <TouchableOpacity activeOpacity={.7} style = {tw`mx-1`} >
               <SimpleLineIcons name="camera" size={24} color={Colors.light.tint} />
             </TouchableOpacity>
-            <TouchableOpacity style = {tw`mx-1`} >
+            <TouchableOpacity activeOpacity={.7} style = {tw`mx-1`} >
               <MaterialIcons name="keyboard-voice" size={24} color={Colors.light.tint} />
             </TouchableOpacity>
           </View>):(
             <View>
-              <TouchableOpacity style = {tw` ml-1 p-1 flex items-center justify-center rounded-full h-7 w-7 bg-[${Colors.light.tint}]`}>
+              <TouchableOpacity activeOpacity={.7} style = {tw` ml-1 p-1 flex items-center justify-center rounded-full h-7 w-7 bg-[${Colors.light.tint}]`}>
                 <Ionicons name="send" size={18} color={'#fff'} style={tw`ml-1`} />
               </TouchableOpacity>
             </View>
